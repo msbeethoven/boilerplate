71da74da01346993449afa381c1aee587b790698
core <seorisachs@gmail.com>
2019-07-05 13:18:11 -0400

    Set up

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: ['babel-polyfill', './client/app.js'],
+  mode: 'development',
+  output: {
+    path: __dirname,
+    filename: 'public/bundle.js'
+  },
+  devtool: 'source-maps',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
+
