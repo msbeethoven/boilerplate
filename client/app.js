d2726246fd91910cc492e1f04a291cb15a7017ce
core <seorisachs@gmail.com>
2019-07-06 00:23:14 -0400

    Redux set up. Commented out the Provider. When using Redux, uncomment that.

--- client/app.js
+++ client/app.js
@@ -1,3 +1,15 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import store from './redux/store';
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <div>Hello world! </div>
+// </Provider>,
+//   document.getElementById('app')
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
---
+++ client/redux/reducer.js
@@ -0,0 +1,10 @@
+//import {constants} from './constants';
+
+const dummyReducer = (state = {}, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer; 
