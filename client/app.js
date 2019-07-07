61bcf8f2dede4f7adf603e9b0811791205b8ff34
core <seorisachs@gmail.com>
2019-07-07 17:58:03 -0400

    Redux added

--- client/app.js
+++ client/app.js
@@ -1,20 +1,20 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
-// import store from './redux/store';
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <div>Hello world! </div>
-// </Provider>,
-//   document.getElementById('app')
-// );
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.render(
-
-  <div> Hello world! </div>,
+  <Provider store={store}>
+    <div>Hello world! </div>
+</Provider>,
   document.getElementById('app')
-)
+);
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// ReactDOM.render(
+
+//   <div> Hello world! </div>,
+//   document.getElementById('app')
+// )
--- client/redux/reducer.js
+++ client/redux/reducer.js
@@ -7,4 +7,4 @@ const dummyReducer = (state = {}, action) => {
   }
 };
 
-export default reducer; 
+export default dummyReducer; 
